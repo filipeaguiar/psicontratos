463a8ff417a1dabcfd1f3866e918f5bd4a102faf
filipeaguiar <filipeaguiar@users.noreply.github.com>
2025-06-28 00:55:46 +0000

    deploy: 284292058eeb06757ead7d58db0de59585ee9645

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/psicontratos/sw.js', { scope: '/psicontratos/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
